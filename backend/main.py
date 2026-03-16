293fe56ee254c6aff47f26440ad0ad5a215ca6bc
LEE JI MIN <min051217@ajou.ac.kr>
2026-03-16 11:06:59 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -18,6 +18,13 @@ print("YOUTUBE loaded:", bool(YOUTUBE_API_KEY))
 print("SPOTIFY CLIENT ID loaded:", bool(SPOTIFY_CLIENT_ID))
 print("SPOTIFY CLIENT SECRET loaded:", bool(SPOTIFY_CLIENT_SECRET))
 
+#PowerShell 코드 터미널에 입력하면 YouTube API 호출 테스트 실행 가능
+#$line = Get-Content .env | Where-Object { $_ -match '^YOUTUBE_API_KEY=' } | Select-Object -First 1
+#$env:YOUTUBE_API_KEY = ($line -split '=', 2)[1].Trim()
+#(Invoke-RestMethod "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=1&q=music&key=$env:YOUTUBE_API_KEY").items[0].snippet.title
+
+
+#터미널에 .\.venv\Scripts\python -m uvicorn backend.main:app --reload 입력하면 서버 실행 가능
 app = FastAPI()
 
 @app.get("/")
