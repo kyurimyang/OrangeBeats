31f532fc7b99456fa00b74a756438f10dc19e1e7
kyurimyang <kyurim1006@gmail.com>
2026-03-25 10:36:22 +0900

    오류 수정

--- app/services/youtube_service.py
+++ app/services/youtube_service.py
@@ -1,11 +1,11 @@
 # 유튜브에서 설명란/댓글 모아서 한 번에 반환
 
-from app.clients.youtube_client import collect_youtube_texts
+from app.clients.youtube_client import collect_text_sources
 from app.constants.pipeline_params import COMMENT_LIMIT_DEFAULT
 
 
-def collect_text_sources(url: str) -> dict:
-    data = collect_youtube_texts(url)
+def collect_text_final(url: str) -> dict:
+    data = collect_text_sources(url)
 
     return {
         "input_url": url,
