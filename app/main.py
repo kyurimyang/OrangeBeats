00a6902f43ebef3c3be52d5ddf6881d08c073ffe
LEE JI MIN <min051217@ajou.ac.kr>
2026-03-31 07:46:16 +0900

    Revert

--- app/main.py
+++ app/main.py
@@ -1,24 +1,18 @@
-#서버 시작 파일python -m uvicorn app.main:app --reload
-# ``
-# 서버 종료 Ctrl + C
-
+#서버 시작 파일
+# `python -m uvicorn app.main:app --reload`
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers import youtube,spotify,playlist
+from app.routers import youtube
 
-app = FastAPI(title="Orange Beats")
+app = FastAPI(title="Orange Playlist")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:3000",
-        "http://localhost:3000",],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(youtube.router)
-app.include_router(spotify.router)
-app.include_router(playlist.router)
